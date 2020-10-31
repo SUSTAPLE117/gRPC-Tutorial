734316262d282aca4d07c22bc11fed745390fb18
Alexis-Maurer Fortin <sustaple_1@hotmail.com>
2020-10-31 00:59:54 -0400

    first unary method

--- route_guide/server/server.go
+++ route_guide/server/server.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	routeguide "google.golang.org/grpc/examples/route_guide"
+	"google.golang.org/protobuf/proto"
 )
 
 type routeGuideServer struct {
+	savedFeatures []*routeguide.Feature
 }
 
 func (r *routeGuideServer) GetFeature(ctx context.Context, point *routeguide.Point) (*routeguide.Feature, error) {
-	panic("implement me")
+	for _, feature := range r.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	return &routeguide.Feature{Location: point}, nil
 }
 
 func (r *routeGuideServer) ListFeatures(rectangle *routeguide.Rectangle, server routeguide.RouteGuide_ListFeaturesServer) error {
