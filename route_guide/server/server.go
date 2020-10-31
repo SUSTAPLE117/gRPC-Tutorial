2024d34cd33dbf2b2943aa643ecaf93febf2c3a8
Alexis-Maurer Fortin <sustaple_1@hotmail.com>
2020-10-31 01:08:02 -0400

    server side streaming method

--- route_guide/server/server.go
+++ route_guide/server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	routeguide "google.golang.org/grpc/examples/route_guide"
 	"google.golang.org/protobuf/proto"
+	"math"
 )
 
 type routeGuideServer struct {
@@ -20,7 +21,14 @@ func (r *routeGuideServer) GetFeature(ctx context.Context, point *routeguide.Poi
 }
 
 func (r *routeGuideServer) ListFeatures(rectangle *routeguide.Rectangle, server routeguide.RouteGuide_ListFeaturesServer) error {
-	panic("implement me")
+	for _, feature := range r.savedFeatures {
+		if inRange(feature.Location, rectangle) {
+			if err := server.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func (r *routeGuideServer) RecordRoute(server routeguide.RouteGuide_RecordRouteServer) error {
@@ -33,4 +41,19 @@ func (r *routeGuideServer) RouteChat(server routeguide.RouteGuide_RouteChatServe
 
 func (r *routeGuideServer) mustEmbedUnimplementedRouteGuideServer() {
 	panic("implement me")
+}
+
+func inRange(point *routeguide.Point, rect *routeguide.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+
+	if float64(point.Longitude) >= left &&
+		float64(point.Longitude) <= right &&
+		float64(point.Latitude) >= bottom &&
+		float64(point.Latitude) <= top {
+		return true
+	}
+	return false
 }
